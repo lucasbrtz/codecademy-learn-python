693a36ab01a4b8925f2df99badeec296d23c877f
Lucas <bretzothers@gmail.com>
2022-10-17 22:26:32 -0300

    Add advanced control flow exercises

---
+++ challenges/2-advanced-control-flow/3-always-false.py
@@ -0,0 +1,10 @@
+def always_false(num):
+    if num > 1 and num < 1:
+        return True
+    else:
+        return False
+
+
+print(always_false(0))
+print(always_false(-1))
+print(always_false(1))
